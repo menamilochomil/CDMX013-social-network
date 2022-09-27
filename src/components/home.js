486f89b4fb531d5ac7c7b108b397152f09b00f20
menamilochomil <jimenamilochomil@gmail.com>
2022-09-27 16:29:33 -0500

    changes: changes teh console.log to verify the info of id

--- src/components/home.js
+++ src/components/home.js
@@ -88,8 +88,8 @@ export const home = () => {
     deteleTrash.forEach((trash) => {
       trash.addEventListener('click', ({ target: { dataset } }) => {
         if (window.confirm('Do you really want to delete?')) {
-          deletePost(dataset.id);
           console.log(dataset.id);
+          deletePost(dataset.id);
         }
       });
     });
